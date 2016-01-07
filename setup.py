dd815ce76ac882f4ec303791a02a04e765c36e90
Tyler Crumpton <tyler.crumpton@synapse-wireless.com>
2016-01-07 10:36:53 -0600

    Add IO includes for Pyduino.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='pyduino-includes',
+    description="Basic SNAPpy includes for the Synapse Pyduino wireless meshing development board",
+    maintainer='Tyler Crumpton',
+    maintainer_email='tyler.crumpton@synapse-wireless.com',
+    url='https://git.synapse-wireless.com/tyler.crumpton/pyduino-includes',
+    packages=['PyduinoIncludes'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Natural Language :: English',
+    ],
+)
